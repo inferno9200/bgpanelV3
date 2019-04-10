1ebf3c1d3e85854295b60c64726c1629f8d82426
ƬHE ЯAW ☣ <raw@dopehosting.net>
2019-04-10 18:04:36 +0200

    Update utilitiesoptimize.php

--- admin/utilitiesoptimize.php
+++ admin/utilitiesoptimize.php
@@ -1,34 +1,4 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
-
-/**
- * LICENSE:
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
- * @categories	Games/Entertainment, Systems Administration
- * @package		Bright Game Panel
- * @author		warhawk3407 <warhawk3407@gmail.com> @NOSPAM
- * @copyleft	2013
- * @license		GNU General Public License version 3.0 (GPLv3)
- * @version		(Release 0) DEVELOPER BETA 8
- * @link		http://www.bgpanel.net/
- */
-
-
-
-
 $page = 'utilitiesoptimize';
 $tab = 4;
 $return = 'utilitiesoptimize.php';
@@ -46,10 +16,10 @@ $title = T_('Optimize Database');
 /* ANALYZE BGP TABLES */
 function analyze_database()
 {
-	$result = mysql_query('SHOW TABLES');
+	$result = mysqli_query($conn, 'SHOW TABLES');
 	$i = 0;
 
-	while($table = mysql_fetch_row($result))
+	while($table = mysqli_fetch_row($result))
 	{
 		if (preg_match("#^".DBPREFIX."#", $table[0]))
 		{
