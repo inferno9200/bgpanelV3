5a28f021b5e44c3502b6a571fba8a45c76136829
ƬHE ЯAW ☣ <raw@dopehosting.net>
2019-04-10 17:45:20 +0200

    Update serverlog.php

--- admin/serverlog.php
+++ admin/serverlog.php
@@ -1,33 +1,4 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
-
-/**
- * LICENSE:
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
- * @categories	Games/Entertainment, Systems Administration
- * @package		Bright Game Panel
- * @author		warhawk3407 <warhawk3407@gmail.com> @NOSPAM
- * @copyleft	2013
- * @license		GNU General Public License version 3.0 (GPLv3)
- * @version		(Release 0) DEVELOPER BETA 8
- * @link		http://www.bgpanel.net/
- */
-
-
-
 $page = 'serverlog';
 $tab = 2;
 $isSummary = TRUE;
@@ -42,8 +13,6 @@ else
 }
 ###
 $return = 'serverlog.php?id='.urlencode($serverid);
-
-
 require("../configuration.php");
 require("./include.php");
 
@@ -72,7 +41,7 @@ $numPages = ceil($numLogs / 50);
 
 if (isset($_GET['page']))
 {
-	$page = mysql_real_escape_string($_GET['page']);
+	$page = mysqli_real_escape_string($conn, $_GET['page']);
 }
 else
 {
@@ -88,7 +57,7 @@ if ($page > 15 || !is_numeric($page))
 //---------------------------------------------------------+
 //Logs:
 
-$logs = mysql_query( "SELECT * FROM `".DBPREFIX."log` WHERE `serverid` = '".$serverid."' ORDER BY `logid` DESC LIMIT ".(($page - 1) * 50).", 50" );
+$logs = mysqli_query($conn, "SELECT * FROM `".DBPREFIX."log` WHERE `serverid` = '".$serverid."' ORDER BY `logid` DESC LIMIT ".(($page - 1) * 50).", 50" );
 
 //---------------------------------------------------------+
 
@@ -168,7 +137,7 @@ for ($i = 1; $i < $numPages + 1; $i++)
 					<tbody>
 <?php
 
-if (mysql_num_rows($logs) == 0)
+if (mysqli_num_rows($logs) == 0)
 {
 ?>
 						<tr>
@@ -178,7 +147,7 @@ if (mysql_num_rows($logs) == 0)
 }
 
 $n = 0;
-while ($rowsLogs = mysql_fetch_assoc($logs))
+while ($rowsLogs = mysqli_fetch_assoc($logs))
 {
 ?>
 						<tr>
@@ -198,7 +167,7 @@ unset($n);
 				</table>
 <?php
 
-if (mysql_num_rows($logs) != 0)
+if (mysqli_num_rows($logs) != 0)
 {
 ?>
 				<script type="text/javascript">
