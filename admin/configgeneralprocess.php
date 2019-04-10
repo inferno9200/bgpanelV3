c9438aadbf97595fe04330294fef6ec059daf46e
ƬHE ЯAW ☣ <raw@dopehosting.net>
2019-04-10 17:15:31 +0200

    Update configgeneralprocess.php

--- admin/configgeneralprocess.php
+++ admin/configgeneralprocess.php
@@ -1,33 +1,4 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
-
-/**
- * LICENSE:
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
- * @categories	Games/Entertainment, Systems Administration
- * @package		Bright Game Panel
- * @author		warhawk3407 <warhawk3407@gmail.com> @NOSPAM
- * @copyleft	2013
- * @license		GNU General Public License version 3.0 (GPLv3)
- * @version		(Release 0) DEVELOPER BETA 8
- * @link		http://www.bgpanel.net/
- */
-
-
-
 $return = TRUE;
 
 
@@ -38,22 +9,22 @@ require("../includes/templates.php");
 
 if (isset($_POST['task']))
 {
-	$task = mysql_real_escape_string($_POST['task']);
+	$task = mysqli_real_escape_string($conn, $_POST['task']);
 }
 else if (isset($_GET['task']))
 {
-	$task = mysql_real_escape_string($_GET['task']);
+	$task = mysqli_real_escape_string($conn, $_GET['task']);
 }
 
 
 switch (@$task)
 {
 	case 'generaledit':
-		$panelName = mysql_real_escape_string($_POST['panelName']);
-		$systemUrl = mysql_real_escape_string($_POST['systemUrl']);
-		$adminTemplate = mysql_real_escape_string($_POST['adminTemplate']);
-		$clientTemplate = mysql_real_escape_string($_POST['clientTemplate']);
-		$maintenance = mysql_real_escape_string($_POST['status']);
+		$panelName = mysqli_real_escape_string($conn, $_POST['panelName']);
+		$systemUrl = mysqli_real_escape_string($conn, $_POST['systemUrl']);
+		$adminTemplate = mysqli_real_escape_string($conn, $_POST['adminTemplate']);
+		$clientTemplate = mysqli_real_escape_string($conn, $_POST['clientTemplate']);
+		$maintenance = mysqli_real_escape_string($conn, $_POST['status']);
 		###
 		//Check the inputs. Output an error if the validation failed
 		$panelNameLength = strlen($panelName);
