2b53df05c4415d71c2fae8d8353ec5f1c52d657a
ƬHE ЯAW ☣ <raw@dopehosting.net>
2019-04-10 13:35:19 +0200

    Update func.auth.inc.php

--- includes/func.auth.inc.php
+++ includes/func.auth.inc.php
@@ -1,5 +1,4 @@
 <?php
-//Prevent direct access
 if (!defined('LICENSE'))
 {
 	exit('Access Denied');
@@ -7,20 +6,19 @@ if (!defined('LICENSE'))
 
 function validateAdmin()
 {
-	//this is a security measure
 	session_regenerate_id();
-	###
 	$token = session_id();
 	###
+	$conn = mysqli_connect(DBHOST, DBUSER, DBPASSWORD, DBNAME);
 	mysqli_query($conn, "UPDATE `".DBPREFIX."admin` SET `token` = '".$token."' WHERE `adminid` = '".$_SESSION['adminid']."'" );
 }
 
 function validateClient()
 {
 	session_regenerate_id();
-	###
 	$token = session_id();
 	###
+	$conn = mysqli_connect(DBHOST, DBUSER, DBPASSWORD, DBNAME);
 	mysqli_query($conn, "UPDATE `".DBPREFIX."client` SET `token` = '".$token."' WHERE `clientid` = '".$_SESSION['clientid']."'" );
 }
 
@@ -28,8 +26,9 @@ function isAdminLoggedIn()
 {
 	if (!empty($_SESSION['adminid']) && is_numeric($_SESSION['adminid']))
 	{
+		$conn = mysqli_connect(DBHOST, DBUSER, DBPASSWORD, DBNAME);
 		$adminverify = mysqli_query($conn, "SELECT `username` FROM `".DBPREFIX."admin` WHERE `adminid` = '".$_SESSION['adminid']."' && `status` = 'Active'" );
-		if (mysqli_num_rows($conn, $adminverify) == 1)
+		if (mysqli_num_rows($adminverify) == '1')
 		{
 			return TRUE;
 		}
@@ -42,8 +41,9 @@ function isClientLoggedIn()
 {
 	if (!empty($_SESSION['clientid']) && is_numeric($_SESSION['clientid']))
 	{
+		$conn = mysqli_connect(DBHOST, DBUSER, DBPASSWORD, DBNAME);
 		$clientverify = mysqli_query($conn, "SELECT `username` FROM `".DBPREFIX."client` WHERE `clientid` = '".$_SESSION['clientid']."' && `status` = 'Active'" );
-		if (mysql_num_rows($clientverify) == 1)
+		if (mysqli_num_rows($clientverify) == '1')
 		{
 			return TRUE;
 		}
@@ -54,7 +54,7 @@ function isClientLoggedIn()
 
 function logout()
 {
-	$_SESSION = array(); //Destroy session variables
+	$_SESSION = array();
 	session_destroy();
 }
 
