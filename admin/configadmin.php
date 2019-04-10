f830109b1f9c302450fbdfd917bcf07135709624
ƬHE ЯAW ☣ <raw@dopehosting.net>
2019-04-10 16:59:37 +0200

    Update configadmin.php

--- admin/configadmin.php
+++ admin/configadmin.php
@@ -1,33 +1,4 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
-
-/**
- * LICENSE:
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
- * @categories	Games/Entertainment, Systems Administration
- * @package		Bright Game Panel
- * @author		warhawk3407 <warhawk3407@gmail.com> @NOSPAM
- * @copyleft	2013
- * @license		GNU General Public License version 3.0 (GPLv3)
- * @version		(Release 0) DEVELOPER BETA 8
- * @link		http://www.bgpanel.net/
- */
-
-
-
 $page = 'configadmin';
 $tab = 5;
 $return = 'configadmin.php';
@@ -38,7 +9,7 @@ require("./include.php");
 
 $title = T_('Administrators');
 
-$admins = mysql_query( "SELECT * FROM `".DBPREFIX."admin` ORDER BY `adminid`" );
+$admins = mysqli_query($conn, "SELECT * FROM `".DBPREFIX."admin` ORDER BY `adminid`" );
 
 
 include("./bootstrap/header.php");
@@ -74,7 +45,7 @@ include("./bootstrap/notifications.php");
 					<tbody>
 <?php
 
-while ($rowsAdmins = mysql_fetch_assoc($admins))
+while ($rowsAdmins = mysqli_fetch_assoc($admins))
 {
 ?>
 						<tr>
@@ -95,7 +66,7 @@ while ($rowsAdmins = mysql_fetch_assoc($admins))
 				</table>
 <?php
 
-if (mysql_num_rows($admins) != 0)
+if (mysqli_num_rows($admins) != 0)
 {
 ?>
 				<script type="text/javascript">
