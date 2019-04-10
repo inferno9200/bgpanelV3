dc3eb6693b91d25f0c56da6a6aaefa8d2e419628
ƬHE ЯAW ☣ <raw@dopehosting.net>
2019-04-10 18:04:45 +0200

    Update utilitiesoptimizeprocess.php

--- admin/utilitiesoptimizeprocess.php
+++ admin/utilitiesoptimizeprocess.php
@@ -1,33 +1,4 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
-
-/**
- * LICENSE:
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
- * @categories	Games/Entertainment, Systems Administration
- * @package		Bright Game Panel
- * @author		warhawk3407 <warhawk3407@gmail.com> @NOSPAM
- * @copyleft	2013
- * @license		GNU General Public License version 3.0 (GPLv3)
- * @version		(Release 0) DEVELOPER BETA 8
- * @link		http://www.bgpanel.net/
- */
-
-
-
 $return = TRUE;
 
 
@@ -37,23 +8,23 @@ require("./include.php");
 
 if (isset($_POST['task']))
 {
-	$task = mysql_real_escape_string($_POST['task']);
+	$task = mysqli_real_escape_string($conn, $_POST['task']);
 }
 else if (isset($_GET['task']))
 {
-	$task = mysql_real_escape_string($_GET['task']);
+	$task = mysqli_real_escape_string($conn, $_GET['task']);
 }
 
 
 switch (@$task)
 {
 	case 'optimize':
-		$result = mysql_query('SHOW TABLES');
-		while($table = mysql_fetch_row($result))
+		$result = mysqli_query($conn, 'SHOW TABLES');
+		while($table = mysqli_fetch_row($result))
 		{
 			if (preg_match("#^".DBPREFIX."#", $table[0]))
 			{
-				mysql_query('OPTIMIZE TABLE '.$table[0]);
+				mysqli_query($conn, 'OPTIMIZE TABLE '.$table[0]);
 			}
 		}
 		unset($result);
