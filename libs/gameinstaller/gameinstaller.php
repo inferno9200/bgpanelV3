ad2cf66f00bfe14d513f14c6225e9db7d0f524b5
ƬHE ЯAW ☣ <raw@dopehosting.net>
2019-04-11 14:38:26 +0200

    Update gameinstaller.php

--- libs/gameinstaller/gameinstaller.php
+++ libs/gameinstaller/gameinstaller.php
@@ -1,65 +1,6 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
-
-/**
- * LICENSE:
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
- * @categories	Games/Entertainment, Systems Administration
- * @package		Bright Game Panel
- * @author		Simon Mora <samt2497@hotmail.com>
- * @author		warhawk3407 <warhawk3407@gmail.com> @NOSPAM
- * @copyleft	2013
- * @license		GNU General Public License version 3.0 (GPLv3)
- * @version		(Release 0) DEVELOPER BETA 8
- * @link		http://www.bgpanel.net/
- */
-
-
-/**
- *	@Class:		Game Installer Main Class
- *	@Version:	1.0
- *	@Date:		20/08/2013
- */
 class GameInstaller {
-
-	//------------------------------------------------------------------------------------------------------------+
-	//------------------------------------------------------------------------------------------------------------+
-
-	// VARIABLES
-
-	//------------------------------------------------------------------------------------------------------------+
-	//------------------------------------------------------------------------------------------------------------+
-
-	/**
-	 * SSH2 Object
-	 * Connection To Remote Host
-	 * Must Be Constructed With PHPSECLIB
-	 *
-	 * @see: http://phpseclib.sourceforge.net/
-	 * @var object
-	 * @access private
-	 */
 	private $sshConnection;
-
-	/**
-	 * Tells If A Game Has Been Selected For The Creation Of A Repository
-	 *
-	 * @var bool
-	 * @access public
-	 */
 	public $gameSet = FALSE;
 
 	/**
